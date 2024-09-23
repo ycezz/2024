b029545c0588e9e290e2a6dbbeb79cf1ae4456ad
Putri Mila <putrimiila.fx@gmail.com>
2024-09-23 15:07:15 +0800

    add

---
+++ profile/app/core/App.php
@@ -0,0 +1,12 @@
+<?php 
+
+class App {
+    protected $controller = 'HomeController';
+    protected $method = 'index';
+    protected $params = [];
+
+    // public function __construct()
+    // {
+        
+    // }
+}
